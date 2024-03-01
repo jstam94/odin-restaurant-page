deefefd1738f1bb926261b38f482a29992b9cea7
Jordan Stammer <s.jordanstammer@gmail.com>
2024-02-29 23:59:13 -0500

    Add Style
    Additional Style added. This includes visual indication of which
    page is selected.

--- src/about.js
+++ src/about.js
@@ -1,7 +1,9 @@
 import Shop from './shop.jpg';
 
 export default function about(){
-    let container = document.createElement('div.menu');
+    let container = document.createElement('div');
+    let map = document.createElement('div')
+    map.innerHTML = `<div style="text-decoration:none; overflow:hidden;max-width:100%;width:500px;height:500px;"><div id="g-mapdisplay" style="height:100%; width:100%;max-width:100%;"><iframe style="height:100%;width:100%;border:0;" frameborder="0" src="https://www.google.com/maps/embed/v1/place?q=statue+of+liberty&key=AIzaSyBFw0Qbyq9zTFTd-tUY6dZWTgaQzuU17R8"></iframe></div><a class="google-map-html" href="https://kbj9qpmy.com/hrn" id="make-map-infor-mation">Hosting Right Now</a><style>#g-mapdisplay .text-marker{}.map-generator{max-width: 100%; max-height: 100%; background: none;</style></div>`
     
     const header = document.createElement('h2');
     header.textContent = `About`
@@ -22,6 +24,8 @@ export default function about(){
     let paragraph = document.createElement('p')
     paragraph.textContent = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse';
     container.appendChild(paragraph);
+
+    container.appendChild(map);
     
 
     return container;
--- src/index.js
+++ src/index.js
@@ -1,25 +1,49 @@
+
 import home from './home.js'
 import menu from './menu.js'
 import about from './about.js'
 import './style.css'
 
-const content = document.querySelector('#content')
 
-content.appendChild(home());
-
-
-
-document.querySelector('button#home').addEventListener('click', () =>{
-    content.innerHTML = ``;
+let app = (function(){
+    const content = document.querySelector('#content');
+    const buttons = document.querySelectorAll('button');
+    const homeBtn = document.querySelector('#home')
+    const menuBtn = document.querySelector('#menu')
+    const aboutBtn = document.querySelector('#about')
+    
+    function clearButtons(){
+        buttons.forEach(button => {
+            button.classList.remove('selected'
+            )
+        });
+    }
+    
     content.appendChild(home());
-})
+    
+    
+    
+    
+    homeBtn.addEventListener('click', () =>{
+        content.innerHTML = ``;
+        clearButtons();
+        homeBtn.className = 'selected'
+        content.appendChild(home());
+    })
+    
+    menuBtn.addEventListener('click', () =>{
+        content.innerHTML = ``;
+        clearButtons();
+        menuBtn.className = 'selected'
+        content.appendChild(menu());
+    })
+    
+    aboutBtn.addEventListener('click', () =>{
+        content.innerHTML = ``;
+        clearButtons();
+        aboutBtn.className = 'selected'
+        content.appendChild(about());
+    })
 
-document.querySelector('button#menu').addEventListener('click', () =>{
-    content.innerHTML = ``;
-    content.appendChild(menu());
-})
+})()
 
-document.querySelector('button#about').addEventListener('click', () =>{
-    content.innerHTML = ``;
-    content.appendChild(about());
-})
