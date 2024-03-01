cbd17fc27e4db93fa8720745595ee3551fb0093b
Jordan Stammer <s.jordanstammer@gmail.com>
2024-02-29 21:24:55 -0500

    Menu Content Added
    Place holder content added for menu module. This includes
    functionality to quickly add new menu items

--- src/menu.js
+++ src/menu.js
@@ -4,7 +4,59 @@ export default function menu(){
     const header = document.createElement('h2');
     header.textContent = `Menu`
 
+
     container.appendChild(header)
 
+    let menu = document.createElement('div')
+   
+    addItem('test', 4, `Lorem ipsum dolor sit amet, consectetur adi`)
+
+    addItem('test', 12, `Lorem ipsum dolor sit amet, consectetur adi`)
+
+    addItem('test', 1.5, `Lorem ipsum dolor sit amet, consectetur adi`)
+
+    addItem('test', 3, `Lorem ipsum dolor sit amet, consectetur adi`)
+
+    addItem('test', 12, `Lorem ipsum dolor sit amet, consectetur adi`)
+
+    addItem('test', 5, `Lorem ipsum dolor sit amet, consectetur adi`)
+
+    addItem('test', 4, `Lorem ipsum dolor sit amet, consectetur adi`)
+
+    addItem('test', 1, `Lorem ipsum dolor sit amet, consectetur adi`)
+
+
+    container.appendChild(menu);
+
     return container;
+
+    function addItem(name, price, info){
+        menu.appendChild(menuItem(name, price, info))
+    }
 }
+
+
+function menuItem (name, price, info){
+    let item = document.createElement('li');
+
+    let itemHeader = document.createElement('div');
+
+    let itemName = document.createElement('h4');
+    itemName.textContent = name;
+
+    let itemPrice = document.createElement('span');
+    itemPrice.textContent = price;
+
+    itemHeader.appendChild(itemName);
+    itemHeader.appendChild(itemPrice)
+
+    let itemInfo = document.createElement('p');
+    itemInfo.textContent = info;
+
+    item.appendChild(itemHeader);
+    item.appendChild(itemInfo)
+
+    return item;
+    
+}
+
