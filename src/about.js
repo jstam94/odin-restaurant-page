c1e1369953d76431039bd850ee835c7334e79a2e
Jordan Stammer <s.jordanstammer@gmail.com>
2024-02-29 20:52:26 -0500

    Add Shop Content
    Place holder content added to shop module

--- src/about.js
+++ src/about.js
@@ -1,3 +1,5 @@
+import Shop from './shop.jpg';
+
 export default function about(){
     let container = document.createElement('div.menu');
     
@@ -5,6 +7,15 @@ export default function about(){
     header.textContent = `About`
 
     container.appendChild(header)
+
+    const shop = new Image(640, 360);
+    shop.src = Shop;
+    container.appendChild(shop);
+
+    let paragraph = document.createElement('p')
+    paragraph.textContent = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse';
+    container.appendChild(paragraph);
+    
 
     return container;
 }
